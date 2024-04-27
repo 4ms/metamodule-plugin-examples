85df7a142213b35c8f8f830b04f39dacb214800c
Dan Green <danngreen1@gmail.com>
2024-04-26 17:10:55 -0700

    Add build for Valley (Plateau only)

---
+++ Valley/Valley.cpp
@@ -0,0 +1,16 @@
+#include "Valley.hpp"
+
+// The pluginInstance-wide instance of the Plugin class
+Plugin *pluginInstance;
+
+void init(rack::Plugin *p) {
+  pluginInstance = p;
+  // p->addModel(modelTopograph);
+  // p->addModel(modelUGraph);
+  // p->addModel(modelDexter);
+  p->addModel(modelPlateau);
+  // p->addModel(modelInterzone);
+  // p->addModel(modelAmalgam);
+  // p->addModel(modelFeline);
+  // p->addModel(modelTerrorform);
+}
