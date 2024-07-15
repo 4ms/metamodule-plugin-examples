1bf7515b23507d1b2bab2bea1ecd82e7e73702e5
audiobird <cantrell@madronemusic.com>
2024-07-15 10:05:52 -0700

    Begin adding Sapphire modules. See description
    Component placement issue, see sapphire submodule commit 57e49f61d8f3

---
+++ Sapphire/Sapphire.cpp
@@ -0,0 +1,19 @@
+//***********************************************************************************************
+// Geodesics: A modular collection for VCV Rack by Pierre Collard and Marc Boulé
+//
+// Based on code from the Fundamental plugins by Andrew Belt
+//  and graphics from the Component Library by Wes Milholen
+// See ./LICENSE.txt for all licenses
+// See ./res/fonts/ for font licenses
+//
+//***********************************************************************************************
+
+#include "plugin.hpp"
+
+Plugin *pluginInstance;
+
+void init(Plugin *p) {
+  pluginInstance = p;
+
+  p->addModel(modelElastika);
+}
