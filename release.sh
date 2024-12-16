577b74227aa9593da968e8b32814ea86681afb18
Dan Green <danngreen1@gmail.com>
2024-12-16 13:17:44 -0800

    CI: dont duplciate plugin name in release file

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 echo "Usage:"
 echo "    release.sh PluginName Version"
 echo "    release.sh Bogaudio v1.2.3"
-git tag -a $2 -m "$1-$2"
-git push origin $2
-gh workflow run build_release.yml -f plugin="$1" -f do_release=true -r $2
+git tag -a $1-$2 -m "$1-$2"
+git push origin $1-$2
+gh workflow run build_release.yml -f plugin="$1" -f do_release=true -r $1-$2
